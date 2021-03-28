9baee3da654ada13afaa6f125dc8186dbf97bdf8
shamuel <shamuel66@gmail.com>
2021-03-28 15:15:53 -0600

    user auth

--- api/models/user.js
+++ api/models/user.js
@@ -4,7 +4,14 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
     surname: String,
-    username: String,
+    username: {type: String, unique: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    },
+    password: {type: String, required: true}
 });
 
 module.exports = mongoose.model('User', userSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 router.get('/',(req, res, next) => {
@@ -17,22 +19,82 @@ router.get('/',(req, res, next) => {
         });
     });
 });
-router.post('/',(req, res, next) => {
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        surname: req.body.surname,
-        username: req.body.username
-    });
-    user
-    .save()
-    .then(result => {
-        console.log(result)
+router.post('/signup',(req, res, next) => {
+    User.find({$or:[{email: req.body.email},{username: req.body.username}]})
+    .exec()
+    .then(user => {
+        if(user.length >= 1){
+            return res.status(409).json({
+                message: 'email address or username already exists'
+            });
+        } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    return  res.json(500)({
+                        error: err
+                    });
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        name: req.body.name,
+                        surname: req.body.surname,
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user
+                    .save()
+                    .then(result => {
+                        console.log(result);
+                        res.status(200).json({
+                            message: 'User created',
+                            createdUser: user
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({error: err});
+                    });
+                }
+            });
+        }
     })
-    .catch(err => console.log(err));
-    res.status(200).json({
-        message: 'User created',
-        createdUser: user
+});
+router.post('/login',(req, res, next) => {
+    User.find({$or:[{email: req.body.email},{username: req.body.username}]})
+    .exec()
+    .then(user => {
+        if(user.length < 1){
+            return res.status(401).json({
+                message: 'Auth Failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err){
+                return res.status(401).json({
+                    message: 'Auth Failed'
+                });
+            }
+            if(result){
+                const token = jwt.sign(
+                {
+                    email: user[0].email,
+                    userId: user[0]._id
+                },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                }
+                );
+                return res.status(200).json({
+                    message: 'Auth succesful',
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message: 'Auth Failed'
+            });
+        });
     });
 });
 router.get('/:userId',(req, res, next) => { 
